c1a3003b9d2b0472477babc25d1b5fb81c9d39ba
alkampfer <ricci.gianmaria@gmail.com>
2015-04-21 14:04:32 +0200

    Added logging on exception on RebusSagaAdapter

--- Jarvis.Framework.Bus.Rebus.Integration/Adapters/RebusSagaAdapter.cs
+++ Jarvis.Framework.Bus.Rebus.Integration/Adapters/RebusSagaAdapter.cs
@@ -1,4 +1,6 @@
-﻿using Jarvis.Framework.Kernel.Engine;
+﻿using System;
+using Castle.Core.Logging;
+using Jarvis.Framework.Kernel.Engine;
 using Jarvis.Framework.Shared.Messages;
 using Jarvis.NEventStoreEx.CommonDomainEx;
 using Jarvis.NEventStoreEx.CommonDomainEx.Persistence;
@@ -9,20 +11,34 @@ namespace Jarvis.Framework.Bus.Rebus.Integration.Adapters
     public class RebusSagaAdapter<TProcessManager, TMessage> : 
         IHandleMessages<TMessage> where TMessage : IMessage where TProcessManager : class, ISagaEx
     {
-        readonly ISagaRepositoryEx _repository;
-        readonly IProcessManagerListener<TProcessManager> _listener;
-        public RebusSagaAdapter(ISagaRepositoryEx repository, IProcessManagerListener<TProcessManager> listener)
+        private readonly ISagaRepositoryEx _repository;
+        private readonly IProcessManagerListener<TProcessManager> _listener;
+        private readonly ILogger _logger;
+        public RebusSagaAdapter(ISagaRepositoryEx repository, IProcessManagerListener<TProcessManager> listener, ILogger logger)
         {
             _repository = repository;
             _listener = listener;
+            _logger = logger;
         }
 
         public void Handle(TMessage message)
         {
-            var id = _listener.GetCorrelationId(message);
-            var pm = _repository.GetById<TProcessManager>(id);
-            pm.Transition(message);
-            _repository.Save(pm, message.MessageId, null);
+            TProcessManager pm = null;
+            try
+            {
+                var id = _listener.GetCorrelationId(message);
+                
+                pm = _repository.GetById<TProcessManager>(id);
+                pm.Transition(message);
+                _repository.Save(pm, message.MessageId, null);
+            }
+            catch (Exception ex)
+            {
+                _logger.ErrorFormat(ex, "Error dispatching message {0} [{1}] to saga {2}", 
+                    message.GetType().Name, message.Describe(), pm == null ? "null" : pm.GetType().Name );
+                throw;
+            }
+           
         }
     }
 }
