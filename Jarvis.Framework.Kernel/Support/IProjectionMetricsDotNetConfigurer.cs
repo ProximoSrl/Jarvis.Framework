a6e32e5c4cf24d2036977e50c1db0f9cbfa2d474
alkampfergit <ricci.gianmaria@gmail.com>
2015-12-04 18:00:32 +0100

    Fixed metrics bug on slot
    During rebuild no health check is done.
    If a slot has current == null it does not crash the metric.

--- Jarvis.Framework.Kernel/Support/IProjectionMetricsDotNetConfigurer.cs
+++ Jarvis.Framework.Kernel/Support/IProjectionMetricsDotNetConfigurer.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Jarvis.Framework.Shared.Metrics
+namespace Jarvis.Framework.Kernel.Support
 {
     public interface IProjectionStatusLoader
     {
