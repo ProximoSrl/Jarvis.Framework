b36ee8755f5cf2b13b34a509b5c66a9e761643ab
alkampfergit <ricci.gianmaria@gmail.com>
2016-04-15 10:09:08 +0200

    Converted SnapshotHelper from stream to function call

--- Jarvis.Framework.Kernel/Store/SnapshotsHelper.cs
+++ Jarvis.Framework.Kernel/Store/SnapshotsHelper.cs
@@ -1,12 +1,12 @@
-﻿//using MongoDB.Bson;
-//using MongoDB.Driver;
-
-//namespace Jarvis.Framework.Kernel.Store
-//{
-//    public static class MongoDbSnapshotsHelper 
-//    {
-//        public static void Drop(IMongoDatabase eventStoreDb)
-//        {
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Jarvis.Framework.Shared.Helpers;
+namespace Jarvis.Framework.Kernel.Store
+{
+    public static class MongoDbSnapshotsHelper
+    {
+        public static void Drop(IMongoDatabase eventStoreDb)
+        {
 //            var script = new BsonJavaScript(
 //@"db.Snapshots.remove({});
 //var cursor = db.Streams.find({'SnapshotRevision' : { $gt : 0}}).sort({_id:1});
@@ -21,7 +21,17 @@
 //    }
 //}while(doc != null);");
 
-//            eventStoreDb.Eval(script);
-//        }
-//    }
-//}
+            var streamCollection = eventStoreDb.GetCollection<BsonDocument>("Streams");
+            eventStoreDb.DropCollection("Snapshots");
+            var streamToUpdate = streamCollection.Find(
+                Builders<BsonDocument>.Filter.Gt("SnapshotRevision", 0))
+                .Sort(Builders<BsonDocument>.Sort.Ascending("_id"));
+            foreach (var doc in streamToUpdate.ToEnumerable())
+            {
+                doc["Unsnapshotted"] = doc["HeadRevision"];
+                doc["SnapshotRevision"] = 0;
+                streamCollection.Save(doc, doc["_id"]);
+            }
+        }
+    }
+}
