6b1c6f7e1e9fa8bb4cd80ff2754ba901563f8c15
alkampfergit <ricci.gianmaria@gmail.com>
2016-05-21 10:59:13 +0200

    Support for mongotest connection for log tests

---
+++ Logging/Jarvis.Framework.LoggingTests/GlobalTestInit.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[SetUpFixture]
+public class GlobalSetup
+{
+    [SetUp]
+    public void ShowSomeTrace()
+    {
+        var overrideTestDb = Environment.GetEnvironmentVariable("TEST_MONGODB");
+        if (String.IsNullOrEmpty(overrideTestDb)) return;
+
+        var overrideTestDbQueryString = Environment.GetEnvironmentVariable("TEST_MONGODB_QUERYSTRING");
+        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
+
+        connectionStringsSection.ConnectionStrings["testDb"].ConnectionString = overrideTestDb.TrimEnd('/') + "/{0}" + overrideTestDbQueryString;
+      config.Save();
+        ConfigurationManager.RefreshSection("connectionStrings");
+    }
+}
