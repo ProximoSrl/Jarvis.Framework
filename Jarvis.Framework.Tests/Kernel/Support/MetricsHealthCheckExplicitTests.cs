3a7d77d569369f741138f074c2f0bf1ab405828c
alkampfergit <ricci.gianmaria@gmail.com>
2016-10-27 12:10:18 +0200

    Added health check for queues.

---
+++ Jarvis.Framework.Tests/Kernel/Support/MetricsHealthCheckExplicitTests.cs
@@ -0,0 +1,23 @@
+﻿using Jarvis.Framework.Kernel.Support;
+using Jarvis.Framework.TestHelpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis.Framework.Tests.Kernel.Support
+{
+    [TestFixture]
+    [Explicit]
+    public class MetricsHealthCheckExplicitTests
+    {
+        [Test]
+        public void VerifyCountOfMsmq()
+        {
+            MsmqHealthCheck sut = new MsmqHealthCheck(".\\Private$\\jarvis.health", 100, new TestLogger());
+            var check = sut.Execute();
+        }
+    }
+}
