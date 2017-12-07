40c986eaa37c611d3fe8f0cdbf799f4334bb2de9
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2017-12-07 15:00:00 +0100

    Fixed rebus error queue handling.

--- Jarvis.Framework.Bus.Rebus.Integration/Support/BusBootstrapper.cs
+++ Jarvis.Framework.Bus.Rebus.Integration/Support/BusBootstrapper.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
-using Jarvis.Framework.Shared.Commands;
 using Jarvis.Framework.Shared.Messages;
 using Jarvis.Framework.Shared.ReadModel;
 using Newtonsoft.Json;
@@ -20,14 +17,14 @@ using Jarvis.Framework.Shared.Domain.Serialization;
 using Rebus.Pipeline;
 using Rebus.Retry;
 using Rebus.Transport;
-using System.Threading.Tasks;
 using Rebus.Retry.Simple;
 using Rebus.Persistence.InMem;
 using Rebus.Routing.TypeBased;
+using Rebus.Retry.PoisonQueues;
 
 namespace Jarvis.Framework.Bus.Rebus.Integration.Support
 {
-	public class BusBootstrapper : IStartable
+	public partial class BusBootstrapper : IStartable
 	{
 		private readonly IWindsorContainer _container;
 		private readonly JarvisRebusConfiguration _configuration;
@@ -39,7 +36,7 @@ namespace Jarvis.Framework.Bus.Rebus.Integration.Support
 		public static readonly JsonSerializerSettings JsonSerializerSettingsForRebus = new JsonSerializerSettings
 		{
 			TypeNameHandling = TypeNameHandling.All,
-			TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Full,
+			TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Full,
 			ContractResolver = new MessagesContractResolver(),
 			ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
 			Converters = new JsonConverter[]
@@ -115,197 +112,38 @@ at least configure one assembly with messages to be dispatched.";
 			var busConfiguration = global::Rebus.Config.Configure.With(new CastleWindsorContainerAdapter(_container))
 				.Serialization(c => c.UseNewtonsoftJson(JsonSerializerSettingsForRebus))
 				.Timeouts(t => t.StoreInMongoDb(_mongoDatabase, _configuration.Prefix + "-timeouts"))
-				.Subscriptions(s => s.StoreInMongoDb(_mongoDatabase, _configuration.Prefix + "-subscriptions", isCentralized : _configuration.CentralizedConfiguration))
+				.Subscriptions(s => s.StoreInMongoDb(_mongoDatabase, _configuration.Prefix + "-subscriptions", isCentralized: _configuration.CentralizedConfiguration))
 				.Events(e => e.BeforeMessageSent += BeforeMessageSent);
 
+			var errorHandler = new JarvisFrameworkErrorHandler(_configuration, JsonSerializerSettingsForRebus, _container, Logger);
+
+			//Important, register IErrorHandler before RetryStrategy or it will register default one.
 			busConfiguration = busConfiguration
-				.Options(o => o.Register<IErrorHandler>(c => new JarvisFrameworkErrorHandler(JsonSerializerSettingsForRebus, _container)));
+				.Options(o => o.Register<IErrorHandler>(c => errorHandler));
+
+			busConfiguration
+				.Options(o =>
+				{
+					o.SetNumberOfWorkers(_configuration.NumOfWorkers);
+					o.Decorate<ITransport>(c =>
+					{
+						var transport = c.Get<ITransport>();
+						_configuration.TransportAddress = transport.Address;
+						errorHandler.SetTransport(transport);
+						return transport;
+					});
+				});
 
 			busConfiguration = busConfiguration
 				.Transport(t => t.UseMsmq(_configuration.InputQueue))
 				.Options(o => o.SimpleRetryStrategy(
 					errorQueueAddress: _configuration.ErrorQueue,
 					maxDeliveryAttempts: _configuration.MaxRetry
-				 )).Options(o =>
-				 {
-					 o.SetNumberOfWorkers(_configuration.NumOfWorkers);
-					 o.Decorate<ITransport>(c => 
-					 {
-						 var transport = c.Get<ITransport>();
-						 _configuration.TransportAddress = transport.Address;
-						 return transport;
-					 });
-				 })
+				 ))
 				.Routing(r => router.Configure(r.TypeBased()));
 
 			//TODO: REBUS4 - manage configuration.ExplicitSubscriptions
 			return busConfiguration;
-		}
-
-		private class JarvisFrameworkErrorHandler : IErrorHandler
-		{
-			const string JsonContentTypeName = "text/json";
-			private readonly JsonSerializerSettings _jsonSerializerSettings;
-			private readonly IWindsorContainer _container;
-
-			private readonly Lazy<IBus> _lazyBus;
-			private readonly Lazy<IMessagesTracker> _lazyMessageTracker;
-
-			public JarvisFrameworkErrorHandler(
-				JsonSerializerSettings jsonSerializerSettings,
-			   IWindsorContainer container)
-			{
-				_jsonSerializerSettings = jsonSerializerSettings;
-				_container = container;
-				_lazyBus = new Lazy<IBus>(() => _container.Resolve<IBus>());
-				_lazyMessageTracker = new Lazy<IMessagesTracker>(() => _container.Resolve<IMessagesTracker>());
-			}
-
-			public async Task HandlePoisonMessage(TransportMessage transportMessage, ITransactionContext transactionContext, Exception exception)
-			{
-				if (transportMessage.Headers.ContainsKey("command.id"))
-				{
-					Guid commandId = Guid.Parse(transportMessage.Headers["command.id"]);
-					var description = transportMessage.Headers["command.description"];
-					String exMessage = description;
-
-					while (exception is TargetInvocationException)
-					{
-						exception = exception.InnerException;
-					}
-
-					if (exception != null)
-					{
-						exMessage = exception.Message;
-					}
-
-					var command = GetCommandFromMessage(transportMessage);
-					_lazyMessageTracker.Value.Failed(command, DateTime.UtcNow, exception);
-
-					if (command != null)
-					{
-						var notifyTo = command.GetContextData(MessagesConstants.ReplyToHeader);
-
-						if (!string.IsNullOrEmpty(notifyTo))
-						{
-							var commandHandled = new CommandHandled(
-									notifyTo,
-									commandId,
-									CommandHandled.CommandResult.Failed,
-									description,
-									exMessage
-									);
-
-							commandHandled.CopyHeaders(command);
-							await _lazyBus.Value.Advanced.Routing.Send(
-								transportMessage.Headers["rebus-return-address"],
-							   commandHandled).ConfigureAwait(false);
-						}
-					}
-				}
-			}
-
-			private ICommand GetCommandFromMessage(TransportMessage message)
-			{
-				var deserializedMessage = Deserialize(message);
-				if (deserializedMessage != null && (deserializedMessage.Body as Object[])?.Length > 0)
-				{
-					var command = (deserializedMessage.Body as Object[])[0] as ICommand;
-					if (command != null) return command;
-				}
-
-				string body;
-				switch (message.Headers["rebus-encoding"].ToString().ToLowerInvariant())
-				{
-					case "utf-7":
-						body = Encoding.UTF7.GetString(message.Body);
-						break;
-					case "utf-8":
-						body = Encoding.UTF8.GetString(message.Body);
-						break;
-					case "utf-32":
-						body = Encoding.UTF32.GetString(message.Body);
-						break;
-					case "ascii":
-						body = Encoding.ASCII.GetString(message.Body);
-						break;
-					case "unicode":
-						body = Encoding.Unicode.GetString(message.Body);
-						break;
-					default:
-						return null;
-				}
-
-				var msg = JsonConvert.DeserializeObject(body, _jsonSerializerSettings);
-				var array = msg as Object[];
-				if (array != null)
-				{
-					return array[0] as ICommand;
-				}
-
-				return null;
-			}
-
-			private Message Deserialize(TransportMessage transportMessage)
-			{
-				var headers = new Dictionary<String, String>(transportMessage.Headers);
-				var encodingToUse = GetEncodingOrThrow(headers);
-
-				var serializedTransportMessage = encodingToUse.GetString(transportMessage.Body);
-				try
-				{
-					var messages = (object[])JsonConvert.DeserializeObject(serializedTransportMessage, _jsonSerializerSettings);
-
-					return new Message(headers, messages);
-				}
-				catch (Exception e)
-				{
-					throw new ArgumentException(
-						string.Format(
-							"An error occurred while attempting to deserialize JSON text '{0}' into an object[]",
-							serializedTransportMessage), e);
-				}
-			}
-
-			Encoding GetEncodingOrThrow(IDictionary<string, String> headers)
-			{
-				if (!headers.ContainsKey(Headers.ContentType))
-				{
-					throw new ArgumentException(
-						string.Format("Received message does not have a proper '{0}' header defined!",
-									  Headers.ContentType));
-				}
-
-				var contentType = (headers[Headers.ContentType] ?? "").ToString();
-				if (!JsonContentTypeName.Equals(contentType, StringComparison.InvariantCultureIgnoreCase))
-				{
-					throw new ArgumentException(
-						string.Format(
-							"Received message has content type header with '{0}' which is not supported by the JSON serializer!",
-							contentType));
-				}
-
-				if (!headers.ContainsKey(Headers.ContentEncoding))
-				{
-					throw new ArgumentException(
-						string.Format(
-							"Received message has content type '{0}', but the corresponding '{1}' header was not present!",
-							contentType, Headers.ContentEncoding));
-				}
-
-				var encodingWebName = (headers[Headers.ContentEncoding] ?? "").ToString();
-
-				try
-				{
-					return Encoding.GetEncoding(encodingWebName);
-				}
-				catch (Exception e)
-				{
-					throw new ArgumentException(
-						string.Format("An error occurred while attempting to treat '{0}' as a proper text encoding",
-									  encodingWebName), e);
-				}
-			}
 		}
 
 		/// <summary>
---
+++ Jarvis.Framework.Bus.Rebus.Integration/Support/JarvisFrameworkRebusErrorHandler.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Castle.Windsor;
+using Jarvis.Framework.Shared.Commands;
+using Jarvis.Framework.Shared.Messages;
+using Jarvis.Framework.Shared.ReadModel;
+using Newtonsoft.Json;
+using Rebus.Bus;
+using Rebus.Messages;
+using Castle.Core.Logging;
+using Rebus.Retry;
+using Rebus.Transport;
+using System.Threading.Tasks;
+using System.Net.Mime;
+
+namespace Jarvis.Framework.Bus.Rebus.Integration.Support
+{
+	public partial class BusBootstrapper
+	{
+		private class JarvisFrameworkErrorHandler : IErrorHandler
+		{
+			const string JsonContentTypeName = "application/json";
+			private readonly JsonSerializerSettings _jsonSerializerSettings;
+			private readonly IWindsorContainer _container;
+
+			private readonly Lazy<IBus> _lazyBus;
+			private readonly Lazy<IMessagesTracker> _lazyMessageTracker;
+			private readonly ILogger _logger;
+			private ITransport _transport;
+			private readonly JarvisRebusConfiguration _jarvisRebusConfiguration;
+
+			public JarvisFrameworkErrorHandler(
+				JarvisRebusConfiguration jarvisRebusConfiguration,
+				JsonSerializerSettings jsonSerializerSettings,
+				IWindsorContainer container,
+				ILogger logger)
+			{
+				_jsonSerializerSettings = jsonSerializerSettings;
+				_container = container;
+				_lazyBus = new Lazy<IBus>(() => _container.Resolve<IBus>());
+				_lazyMessageTracker = new Lazy<IMessagesTracker>(() => _container.Resolve<IMessagesTracker>());
+				_logger = logger;
+				_jarvisRebusConfiguration = jarvisRebusConfiguration;
+			}
+
+			public void SetTransport(ITransport transport)
+			{
+				_transport = transport;
+			}
+
+			public async Task HandlePoisonMessage(TransportMessage transportMessage, ITransactionContext transactionContext, Exception exception)
+			{
+				try
+				{
+					if (transportMessage.Headers.ContainsKey("rbs2-msg-id"))
+					{
+						Guid commandId = Guid.Parse(transportMessage.Headers["rbs2-msg-id"]);
+						var description = transportMessage.Headers["rbs2-msg-type"];
+						String exMessage = description;
+
+						while (exception is TargetInvocationException)
+						{
+							exception = exception.InnerException;
+						}
+
+						if (exception != null)
+						{
+							exMessage = exception.Message;
+						}
+
+						var command = GetCommandFromMessage(transportMessage);
+						_lazyMessageTracker.Value.Failed(command, DateTime.UtcNow, exception);
+
+						if (command != null)
+						{
+							var notifyTo = command.GetContextData(MessagesConstants.ReplyToHeader);
+
+							if (!string.IsNullOrEmpty(notifyTo))
+							{
+								var commandHandled = new CommandHandled(
+										notifyTo,
+										commandId,
+										CommandHandled.CommandResult.Failed,
+										description,
+										exMessage
+										);
+
+								commandHandled.CopyHeaders(command);
+								await _lazyBus.Value.Advanced.Routing.Send(
+									transportMessage.Headers["rbs2-return-address"],
+								   commandHandled).ConfigureAwait(false);
+							}
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					_logger.Error($"Error during HandlePoisonMessage of message: {ex.Message}", ex);
+				}
+				finally
+				{
+					var headers = transportMessage.Headers;
+					headers[Headers.ErrorDetails] = exception?.ToString();
+					headers[Headers.SourceQueue] = _transport.Address;
+					_logger.Error($"Moving message to error queue {_jarvisRebusConfiguration.ErrorQueue}", exception);
+					if (_transport == null)
+					{
+						_logger.Error("Error handler has no transport...this should be not possible. Problem in rebus initialization");
+					}
+					else
+					{
+						await _transport.Send(_jarvisRebusConfiguration.ErrorQueue, transportMessage, transactionContext).ConfigureAwait(false);
+					}
+				}
+			}
+
+			private ICommand GetCommandFromMessage(TransportMessage message)
+			{
+				var deserializedMessage = Deserialize(message);
+
+				if (deserializedMessage == null)
+					return null;
+
+				//check if the body is a command
+				if (deserializedMessage.Body is ICommand)
+					return (ICommand)deserializedMessage.Body;
+
+				//can be an array of messages, takes the first one
+				if ((deserializedMessage.Body as Object[])?.Length > 0)
+				{
+					var command = (deserializedMessage.Body as Object[])?[0] as ICommand;
+					if (command != null) return command;
+				}
+
+				//This is not a command.
+				return null;
+
+				//string body;
+				//switch (message.Headers["rebus-encoding"].ToLowerInvariant())
+				//{
+				//	case "utf-7":
+				//		body = Encoding.UTF7.GetString(message.Body);
+				//		break;
+				//	case "utf-8":
+				//		body = Encoding.UTF8.GetString(message.Body);
+				//		break;
+				//	case "utf-32":
+				//		body = Encoding.UTF32.GetString(message.Body);
+				//		break;
+				//	case "ascii":
+				//		body = Encoding.ASCII.GetString(message.Body);
+				//		break;
+				//	case "unicode":
+				//		body = Encoding.Unicode.GetString(message.Body);
+				//		break;
+				//	default:
+				//		return null;
+				//}
+
+				//var msg = JsonConvert.DeserializeObject(body, _jsonSerializerSettings);
+				//var array = msg as Object[];
+				//if (array != null)
+				//{
+				//	return array[0] as ICommand;
+				//}
+
+				//return null;
+			}
+
+			private Message Deserialize(TransportMessage transportMessage)
+			{
+				var headers = new Dictionary<String, String>(transportMessage.Headers);
+				var encodingToUse = GetEncodingOrThrow(headers);
+
+				var serializedTransportMessage = encodingToUse.GetString(transportMessage.Body);
+				try
+				{
+					Object deserialized = JsonConvert.DeserializeObject(serializedTransportMessage, _jsonSerializerSettings);
+					return new Message(headers, deserialized);
+				}
+				catch (Exception e)
+				{
+					throw new ArgumentException(
+						string.Format(
+							"An error occurred while attempting to deserialize JSON text '{0}' into an object[]",
+							serializedTransportMessage), e);
+				}
+			}
+
+			private Encoding GetEncodingOrThrow(IDictionary<string, String> headers)
+			{
+				if (!headers.ContainsKey(Headers.ContentType))
+				{
+					throw new ArgumentException(
+						string.Format("Received message does not have a proper '{0}' header defined!",
+									  Headers.ContentType));
+				}
+
+				var contentTypeHeaderValue = (headers[Headers.ContentType] ?? "");
+				var contentType = new ContentType(contentTypeHeaderValue);
+
+				if (!contentType.MediaType.StartsWith(JsonContentTypeName, StringComparison.InvariantCultureIgnoreCase))
+				{
+					throw new ArgumentException(
+						string.Format(
+							"Received message has content type header with '{0}' which is not supported by the JSON serializer!",
+							contentTypeHeaderValue));
+				}
+
+				if (String.IsNullOrWhiteSpace(contentType.CharSet))
+				{
+					throw new ArgumentException(
+						string.Format(
+							"Received message has content type '{0}', but it has no charset defined!",
+							contentTypeHeaderValue));
+				}
+
+				try
+				{
+					return Encoding.GetEncoding(contentType.CharSet);
+				}
+				catch (Exception e)
+				{
+					throw new ArgumentException(
+						string.Format("An error occurred while attempting to treat '{0}' as a proper text encoding",
+									  contentType.CharSet), e);
+				}
+			}
+		}
+	}
+}
