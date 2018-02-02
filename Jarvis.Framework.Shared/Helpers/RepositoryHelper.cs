0c39cbe1a6ff7496229f8474275eb6a33f31a468
Alkampfergit <ricci.gianmaria@gmail.com>
2018-02-02 20:11:48 +0100

    RepositoryHelper invoke with reflection sync.
    Changed the reflection call to sync without await.
    
    Invoking with reflection and using await is not
    a good thing.

--- Jarvis.Framework.Shared/Helpers/RepositoryHelper.cs
+++ Jarvis.Framework.Shared/Helpers/RepositoryHelper.cs
@@ -9,75 +9,76 @@ using NStore.Domain;
 
 namespace Jarvis.Framework.Shared.Helpers
 {
-	public static class RepositoryHelper
-	{
-		private static Dictionary<Type, GenericMethodInfo> _getByIdAsync1GenericCache = new Dictionary<Type, GenericMethodInfo>();
-		private static Dictionary<Type, GenericMethodInfo> _saveAsync3GenericCache = new Dictionary<Type, GenericMethodInfo>();
+    public static class RepositoryHelper
+    {
+        private static Dictionary<Type, GenericMethodInfo> _getByIdAsync1GenericCache = new Dictionary<Type, GenericMethodInfo>();
+        private static Dictionary<Type, GenericMethodInfo> _saveAsync3GenericCache = new Dictionary<Type, GenericMethodInfo>();
 
-		public static async Task<IAggregate> GetByIdAsync(this IRepository repository, Type type, string id)
-		{
-			GenericMethodInfo genericMethodInfo;
-			Type repositoryType = repository.GetType();
-			if (!_getByIdAsync1GenericCache.TryGetValue(repositoryType, out genericMethodInfo))
-			{
-				genericMethodInfo = new GenericMethodInfo();
-				genericMethodInfo.GenericMethodInfoDefinition = repositoryType
-					.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-					.Single(_ => _.Name == "GetByIdAsync"
-						&& _.IsGenericMethod
-						&& _.Parameters().Count == 1);
-				_getByIdAsync1GenericCache[repositoryType] = genericMethodInfo;
-			}
+        public static Task<IAggregate> GetByIdAsync(this IRepository repository, Type type, string id)
+        {
+            GenericMethodInfo genericMethodInfo;
+            Type repositoryType = repository.GetType();
+            if (!_getByIdAsync1GenericCache.TryGetValue(repositoryType, out genericMethodInfo))
+            {
+                genericMethodInfo = new GenericMethodInfo();
+                genericMethodInfo.GenericMethodInfoDefinition = repositoryType
+                    .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                    .Single(_ => _.Name == "GetByIdAsync"
+                        && _.IsGenericMethod
+                        && _.Parameters().Count == 1);
+                _getByIdAsync1GenericCache[repositoryType] = genericMethodInfo;
+            }
 
-			MethodInfo minfo = genericMethodInfo.GetByIdAsync1(type);
+            MethodInfo minfo = genericMethodInfo.GetByIdAsync1(type);
 
-			var task = ((Task)minfo.Call(repository, new Object[] { id }));
-			await task.ConfigureAwait(false);
-			return (IAggregate)task.GetPropertyValue("Result");
-		}
+            var task = ((Task)minfo.Call(repository, new Object[] { id }));
 
-		public static async Task SaveAsync(this IRepository repository, Type type, Object aggregate, string operationId, Action<IHeadersAccessor> headers)
-		{
-			GenericMethodInfo genericMethodInfo;
-			Type repositoryType = repository.GetType();
-			if (!_saveAsync3GenericCache.TryGetValue(repositoryType, out genericMethodInfo))
-			{
-				genericMethodInfo = new GenericMethodInfo();
-				genericMethodInfo.GenericMethodInfoDefinition = repositoryType
-					.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-					.Single(_ => _.Name == "SaveAsync"
-						&& _.IsGenericMethod
-						&& _.Parameters().Count == 3);
-				_saveAsync3GenericCache[repositoryType] = genericMethodInfo;
-			}
+            return Task.FromResult((IAggregate)task.GetPropertyValue("Result"));
+        }
 
-			MethodInfo minfo = genericMethodInfo.GetByIdAsync1(type);
+        public static Task SaveAsync(this IRepository repository, Type type, Object aggregate, string operationId, Action<IHeadersAccessor> headers)
+        {
+            GenericMethodInfo genericMethodInfo;
+            Type repositoryType = repository.GetType();
+            if (!_saveAsync3GenericCache.TryGetValue(repositoryType, out genericMethodInfo))
+            {
+                genericMethodInfo = new GenericMethodInfo();
+                genericMethodInfo.GenericMethodInfoDefinition = repositoryType
+                    .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                    .Single(_ => _.Name == "SaveAsync"
+                        && _.IsGenericMethod
+                        && _.Parameters().Count == 3);
+                _saveAsync3GenericCache[repositoryType] = genericMethodInfo;
+            }
 
-			var task = ((Task)minfo.Call(repository, new Object[] { aggregate, operationId, headers }));
-			await task.ConfigureAwait(false);
-		}
+            MethodInfo minfo = genericMethodInfo.GetByIdAsync1(type);
 
-		private class GenericMethodInfo
-		{
-			public GenericMethodInfo()
-			{
-				_internalClosedTypeCache = new Dictionary<Type, MethodInfo>();
-			}
+            var task = ((Task)minfo.Call(repository, new Object[] { aggregate, operationId, headers }));
+            task.Wait();
+            return Task.CompletedTask;
+        }
 
-			public MethodInfo GenericMethodInfoDefinition { get; set; }
+        private class GenericMethodInfo
+        {
+            public GenericMethodInfo()
+            {
+                _internalClosedTypeCache = new Dictionary<Type, MethodInfo>();
+            }
 
-			private readonly Dictionary<Type, MethodInfo> _internalClosedTypeCache;
+            public MethodInfo GenericMethodInfoDefinition { get; set; }
 
-			internal MethodInfo GetByIdAsync1(Type type)
-			{
-				MethodInfo minfo;
-				if (!_internalClosedTypeCache.TryGetValue(type, out minfo))
-				{
-					minfo = GenericMethodInfoDefinition.MakeGenericMethod(type);
-					_internalClosedTypeCache[type] = minfo;
-				}
-				return minfo;
-			}
-		}
-	}
+            private readonly Dictionary<Type, MethodInfo> _internalClosedTypeCache;
+
+            internal MethodInfo GetByIdAsync1(Type type)
+            {
+                MethodInfo minfo;
+                if (!_internalClosedTypeCache.TryGetValue(type, out minfo))
+                {
+                    minfo = GenericMethodInfoDefinition.MakeGenericMethod(type);
+                    _internalClosedTypeCache[type] = minfo;
+                }
+                return minfo;
+            }
+        }
+    }
 }
