bc8b1659cf3db9297040e20fa4d8dfaca7148481
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-05-28 16:21:19 +0200

    Added Call Context for net standard.

--- Jarvis.Framework.Kernel/MultitenantSupport/TenantContext.cs
+++ Jarvis.Framework.Kernel/MultitenantSupport/TenantContext.cs
@@ -1,7 +1,5 @@
 ﻿using Jarvis.Framework.Shared.MultitenantSupport;
 using System;
-
-#if NETFULL
 using System.Runtime.Remoting.Messaging;
 
 namespace Jarvis.Framework.Kernel.MultitenantSupport
@@ -28,30 +26,4 @@ namespace Jarvis.Framework.Kernel.MultitenantSupport
         }
     }
 }
-#else
-namespace Jarvis.Framework.Kernel.MultitenantSupport
-{
-    public static class TenantContext
-    {
-        private const string TenantIdKey = "tenant_id";
 
-        public static void Enter(TenantId id)
-        {
-            throw new NotSupportedException("Not supported in .NET standard");
-        }
-
-        public static TenantId CurrentTenantId
-        {
-            get
-            {
-                throw new NotSupportedException("Not supported in .NET standard");
-            }
-        }
-
-        public static void Exit()
-        {
-            throw new NotSupportedException("Not supported in .NET standard");
-        }
-    }
-}
-#endif
