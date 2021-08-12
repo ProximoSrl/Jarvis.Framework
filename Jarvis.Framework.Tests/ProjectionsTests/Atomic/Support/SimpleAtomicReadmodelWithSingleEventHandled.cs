f97585ed5dfab33216115a5f75396e4a4b8b4f37
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-08-12 17:50:02 +0200

    More test on atomic readmodel

---
+++ Jarvis.Framework.Tests/ProjectionsTests/Atomic/Support/SimpleAtomicReadmodelWithSingleEventHandled.cs
@@ -0,0 +1,31 @@
+﻿using Jarvis.Framework.Shared.ReadModel.Atomic;
+using Jarvis.Framework.Tests.EngineTests;
+using System;
+
+namespace Jarvis.Framework.Tests.ProjectionsTests.Atomic.Support
+{
+    [AtomicReadmodelInfo("SimpleAtomicReadmodelWithSingleEventHandled", typeof(SampleAggregateId))]
+    public class SimpleAtomicReadmodelWithSingleEventHandled : AbstractAtomicReadModel
+    {
+        public SimpleAtomicReadmodelWithSingleEventHandled(string id) : base(id)
+        {
+        }
+
+        public Int32 TouchCount { get; private set; }
+
+#pragma warning disable S1144 // Unused private types or members should be removed
+#pragma warning disable S1172 // Unused method parameters should be removed
+
+        private void On(SampleAggregateTouched _)
+        {
+            TouchCount += 1;
+        }
+#pragma warning restore S1172 // Unused method parameters should be removed
+#pragma warning restore S1144 // Unused private types or members should be removed
+
+        protected override int GetVersion()
+        {
+            return 1;
+        }
+    }
+}
