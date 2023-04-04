a104f9980937b7188b97d7b11754cd67496a9eb4
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2023-04-04 10:04:37 +0200

    Removed residual configuration for cached repository

--- Jarvis.Framework.Shared/JarvisFrameworkGlobalConfiguration.cs
+++ Jarvis.Framework.Shared/JarvisFrameworkGlobalConfiguration.cs
@@ -4,14 +4,6 @@ namespace Jarvis.Framework.Shared
 {
     public static class JarvisFrameworkGlobalConfiguration
     {
-        /// <summary>
-        /// If true the RepositoryCommandHandler will use SingleAggregateRepository cached
-        /// to reuse the same Repository+Aggregate if found in cache. This form of aggressive
-        /// cache can really improve performances when lots of commands are send to the very
-        /// same aggregate.
-        /// </summary>
-        public static Boolean SingleAggregateRepositoryCacheEnabled { get; private set; }
-
         public static Boolean OfflineEventsReadmodelIdempotencyCheck { get; private set; }
 
         public static Boolean AtomicProjectionEngineOptimizedCatchup { get; private set; }
@@ -23,23 +15,12 @@ namespace Jarvis.Framework.Shared
 
         static JarvisFrameworkGlobalConfiguration()
         {
-            SingleAggregateRepositoryCacheEnabled = false;
             AtomicProjectionEngineOptimizedCatchup = true;
         }
 
         public static void DisableMongoDbAsync()
         {
             MongoDbAsyncDisabled = true;
-        }
-
-        public static void DisableSingleAggregateRepositoryCache()
-        {
-            SingleAggregateRepositoryCacheEnabled = false;
-        }
-
-        public static void EnableSingleAggregateRepositoryCache()
-        {
-            SingleAggregateRepositoryCacheEnabled = true;
         }
 
         public static void EnableOfflineEventsReadmodelIdempotencyCheck()
